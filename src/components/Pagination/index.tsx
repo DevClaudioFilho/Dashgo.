757f878aff58dfdfec6fdfbad2fb05b926b30d71
DevClaudioFilho <devclaudiofilho@gmail.com>
2021-09-10 22:03:05 +0100

    feat-adding a pagination in the project

--- src/components/Pagination/index.tsx
+++ src/components/Pagination/index.tsx
@@ -1,7 +1,44 @@
-import { Box, Stack } from "@chakra-ui/react";
+import { Box, Stack, Text } from "@chakra-ui/react";
 import PaginationItem from "./PaginationItem";
 
-export default function Pagination() {
+interface PaginationProps {
+  totalCountOfRegisters: number;
+  registersPerPage?: number;
+  currentPage?: number;
+  onPageChange: (page: number) => void;
+}
+
+const siblingsCount = 1;
+
+function generatePagesArray(from: number, to: number) {
+  return [...new Array(to - from)]
+    .map((_, index) => {
+      return from + index + 1;
+    })
+    .filter((page) => page > 0);
+}
+
+export default function Pagination({
+  totalCountOfRegisters,
+  registersPerPage = 10,
+  onPageChange,
+  currentPage = 1,
+}: PaginationProps) {
+  const lastPage = Math.floor(totalCountOfRegisters / registersPerPage);
+
+  const previousPage =
+    currentPage > 1
+      ? generatePagesArray(currentPage - 1 - siblingsCount, currentPage - 1)
+      : [];
+
+  const nextPage =
+    currentPage < lastPage
+      ? generatePagesArray(
+          currentPage,
+          Math.min(currentPage + siblingsCount, lastPage)
+        )
+      : [];
+
   return (
     <Stack
       direction={["column", "row"]}
@@ -14,10 +51,45 @@ export default function Pagination() {
         <strong>0</strong> - <strong>5</strong> de <strong>100</strong>
       </Box>
       <Stack direction="row" spacing="2">
-        <PaginationItem number={1} isCurrent />
-        <PaginationItem number={2} />
-        <PaginationItem number={3} />
-        <PaginationItem number={4} />
+        {currentPage > 1 + siblingsCount && <PaginationItem number={1} />}
+
+        {previousPage.length > 0 &&
+          previousPage.map((page) => {
+            return (
+              <>
+                <PaginationItem key={page} number={page} />
+                {currentPage > 2 + siblingsCount && (
+                  <Text color="gray.300" width="6" textAlign="center">
+                    ...
+                  </Text>
+                )}
+              </>
+            );
+          })}
+
+        <PaginationItem number={currentPage} isCurrent />
+        {nextPage.length > 0 &&
+          nextPage.map((page) => {
+            return <PaginationItem key={page} number={page} />;
+          })}
+
+        {currentPage + siblingsCount < lastPage && (
+          <PaginationItem number={lastPage} />
+        )}
+
+        {previousPage.length < lastPage &&
+          previousPage.map((page) => {
+            return (
+              <>
+                {currentPage + 1 + siblingsCount < lastPage && (
+                  <Text color="gray.300" width="6" textAlign="center">
+                    ...
+                  </Text>
+                )}
+                <PaginationItem key={page} number={page} />
+              </>
+            );
+          })}
       </Stack>
     </Stack>
   );
--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -16,33 +16,16 @@ import {
   Spinner,
 } from "@chakra-ui/react";
 import Link from "next/link";
-import { useQuery } from "react-query";
 
 import { RiAddLine, RiPencilLine } from "react-icons/ri";
 
 import { Header } from "../../components/Header";
 import Pagination from "../../components/Pagination";
 import { SideBar } from "../../components/SideBar";
+import { useUser } from "../../services/hooks/useUsers";
 
 export default function UserList() {
-  const { data, isLoading, error } = useQuery("users", async () => {
-    const response = await fetch("http://localhost:3000/api/users");
-    const data = await response.json();
-
-    const users = data.users.map((user) => {
-      return {
-        id: user.id,
-        name: user.name,
-        email: user.email,
-        createdAt: new Date(user.createdAt).toLocaleDateString("pt-BR", {
-          day: "2-digit",
-          month: "long",
-          year: "numeric",
-        }),
-      };
-    });
-    return users;
-  });
+  const { data, isLoading, error, isFetching } = useUser();
 
   const isWideVersion = useBreakpointValue({
     base: false,
@@ -59,6 +42,9 @@ export default function UserList() {
           <Flex mb="8" justify="space-between" align="center">
             <Heading size="lg" fontWeight="normal">
               Usuarios
+              {!isLoading && isFetching ? (
+                <Spinner size="sm" color="gray.500" ml="4" />
+              ) : null}
             </Heading>
             <Link href="/users/create" passHref>
               <Button
@@ -125,7 +111,11 @@ export default function UserList() {
                   })}
                 </Tbody>
               </Table>
-              <Pagination />
+              <Pagination
+                totalCountOfRegisters={200}
+                currentPage={4}
+                onPageChange={() => {}}
+              />
             </>
           )}
         </Box>
